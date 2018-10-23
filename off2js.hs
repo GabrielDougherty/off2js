237d8f09c2cd9341bfe05be042bb4001bf6b366e
Gabriel <gabrieljosephdougherty@gmail.com>
2018-10-23 19:40:21 -0400

    More work to process input

--- off2js.hs
+++ off2js.hs
@@ -1,3 +1,5 @@
+import Data.List.Split (splitOn)
+
 gabestuff = [[0,0]
             ,[0,1]
             ,[3,4]
@@ -10,9 +12,20 @@ get2d lst index = [x |
 makePolygon allPoints [] = []
 makePolygon allPoints (x:xs) = get2d allPoints x ++ makePolygon allPoints xs
 
--- make triangles using "fan out" approach
-makeTriangles [a,b,c] = [[a,b,c]]
-makeTriangles (x:xs) = (x:nextTwo) : (makeTriangles $ x:afterTwo)
+-- make triangles from polygon using "fan out" approach
+makeTriangles [a,b,c] = [a,b,c]
+makeTriangles (x:xs) = (x:nextTwo) ++ (makeTriangles $ x:afterTwo)
   where nextTwo = take 2 xs
         afterTwo = drop 1 xs
 
+makeFileLines input = splitOn "\n" input
+
+filteredLines [] = []
+filteredLines (x:xs) = if (head x) == '#'
+  then filteredLines xs
+  else x:(filteredLines xs)
+                  
+main = do
+  off <- getLine
+  print off
+  
