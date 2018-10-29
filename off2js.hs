e439eb768a9f71be9f2981318484da8808b6494b
Gabriel <gabrieljosephdougherty@gmail.com>
2018-10-29 17:56:45 -0400

    Properly output triangles to javascript, work on unit cube

--- off2js.hs
+++ off2js.hs
@@ -3,6 +3,7 @@ import System.Environment
 import Control.Monad
 import qualified Data.Attoparsec.ByteString.Char8 as C
 import qualified Data.ByteString.Char8 as B
+import qualified Data.List as L
 
 get2d lst index = [x |
                     (i,x) <- zip [0..] lst,
@@ -58,21 +59,52 @@ parsePoints = do
 
 myPrint x = (putStrLn . show) x
 
-makeBary :: Int -> B.ByteString
-makeBary numTriangles = B.intercalate "," $ replicate numTriangles tri
+makeBary :: Int -> String
+makeBary numTriangles = "[" ++ csv ++ "]"
   where
     tri = "[1,0,0],[0,1,0],[0,0,1]"
+    csv = L.intercalate "," $ replicate numTriangles tri
 
-strList :: [Double] -> B.ByteString
-strList lst = B.intercalate "," $ map (B.pack . show) lst
+bstrList :: [Double] -> String
+bstrList lst =  L.intercalate "," $ map show lst
 
-jscriptModel triangles bc modelName = "function " ++ modelName ++
-  "() {\n\tthis.triangles = " -- TODO: finish this part
+strList :: [Double] -> String
+strList lst = "[" ++ bstrList lst ++ "]"
+
+str2dList :: [[Double]] -> String
+str2dList lst = "[" ++ L.intercalate "," (map strList lst) ++ "]"
+
+jscriptModel :: [[Double]] -> Int -> String -> String
+jscriptModel triangles bcSize modelName = "function " ++ modelName ++
+  "() {\n\tthis.triangles = " ++ str2dList triangles ++ ";" ++
+  "\nthis.bc = " ++ makeBary bcSize  ++ ";" ++ "\n};"
+
+scaleTriangles :: [[Double]] -> [[Double]]
+scaleTriangles triangles = scaled
+  where
+    max = maximum $ map maximum triangles
+    scaled = map (map (* max)) triangles
+
+-- shift a triangle list to all positive values
+shiftPositive :: [[Double]] -> [[Double]]
+shiftPositive triangles = shifted
+  where
+    min = minimum $ map minimum triangles
+    offset = if min < 0
+            then -min
+            else 0
+    shifted = map (map (+ offset)) triangles
+
+-- shift a triangle list from 1,0 dimensions to 0.5,-0.5
+shiftUnitCube :: [[Double]] -> [[Double]]
+shiftUnitCube triangles = shifted
+  where
+    amt = -0.5
+    shifted = map (map (+ amt)) triangles
 
 parseIt :: IO ([[Double]],[[Int]])
 parseIt = do
   contents <- B.getContents
-  myPrint contents
   let filtered = fileSansComments contents
   let res = C.parseOnly parsePoints filtered
   either (error . show) return res
@@ -81,9 +113,6 @@ main = do
   res <- parseIt
   let makeThese p = makePolygon (fst res) p
   let polygons = map (\p -> makeThese p) (snd res)
-  mapM_ myPrint polygons
   let triangles = concat $ map makeTriangles polygons
-  putStrLn "triangles:"
-  mapM_ print triangles
-  print "done"
+  putStrLn $ jscriptModel triangles (length(triangles)) ("BobsBurgers"::String)
 
